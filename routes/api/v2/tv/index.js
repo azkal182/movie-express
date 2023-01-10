9178453010f88d93af64c09e16cc91f65a3a13df
azka <mohazkalarif@gmail.com>
2023-01-10 21:05:48 +0700

    add pagination

--- routes/api/v2/tv/index.js
+++ routes/api/v2/tv/index.js
@@ -1,17 +1,143 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 const admin = require("firebase-admin");
 const serviceAccount = require("../../../../firebase/akun.json");
 admin.initializeApp({
  credential: admin.credential.cert(serviceAccount),
 });
 
-
-
 const db = admin.firestore();
 
+router.get("/data", (req, res) => {
+ let page = req.query.page || 1;
+ if (isNaN(page) || page == "") page = 1;
+ else page = parseInt(page);
+ let perPage = req.query.perPage;
+ if (isNaN(perPage) || perPage == "") perPage = 24;
+ else perPage = parseInt(perPage);
 
-router.get('/', (req, res) => {
+ let dataRef = db.collection("series");
+ let data = dataRef
+  .limit(perPage)
+  .offset((page - 1) * perPage)
+  .get()
+  .then((snapshot) => {
+   let data = [];
+   snapshot.forEach((doc) => {
+    data.push({ id: doc.id });
+   });
+
+   let total = dataRef.get().then((snapshot) => {
+    return snapshot.size;
+   });
+
+   return Promise.all([data, total]);
+  })
+  .then(([data, total]) => {
+   let totalPages = Math.ceil(total / perPage);
+   res.json({ page, perPage, totalPages, results: data });
+  })
+  .catch((err) => {
+   res.status(500).json({ error: err.message });
+  });
+});
+
+router.get("/", (req, res) => {
+
+   let page = req.query.page || 1;
+   if (isNaN(page) || page == "") page = 1;
+   else page = parseInt(page);
+   let perPage = req.query.perPage;
+   if (isNaN(perPage) || perPage == "") perPage = 24;
+   else perPage = parseInt(perPage);
+
+   let dataRef = db.collection("series");
+   let data = dataRef
+   .orderBy("datePublished", "desc")
+  .limit(perPage)
+  .offset((page - 1) * perPage)
+  .get()
+  .then((snapshot) => {
+   let data = [];
+   snapshot.forEach((doc) => {
+    const selectedData = {
+       name: doc.data().title,
+       id: doc.data().id,
+       datePublished: doc.data().datePublished,
+       director: doc.data().director,
+       categories: doc.data().categories,
+       episode: doc.data().episode,
+       genres: doc.data().genres,
+       poster: doc.data().poster,
+       rating: doc.data().rating,
+       reviewCount: doc.data().reviewCount,
+       trailer: doc.data().trailer,
+       TMDB: doc.data().TMDB,
+      };
+
+      data.push(selectedData);
+    //data.push({ id: doc.id });
+   });
+
+   return Promise.all([data]);
+  })
+  .then(([data]) => {
+   res.json({ page, perPage, results: data });
+  })
+  .catch((err) => {
+   res.status(500).json({ error: err.message });
+  });
+   
+   
+});
+
+router.get("/latest", (req, res) => {
+let page = req.query.page || 1;
+   if (isNaN(page) || page == "") page = 1;
+   else page = parseInt(page);
+   let perPage = req.query.perPage;
+   if (isNaN(perPage) || perPage == "") perPage = 24;
+   else perPage = parseInt(perPage);
+
+   let dataRef = db.collection("series");
+   let data = dataRef
+   .orderBy("datePublished", "desc")
+  .limit(perPage)
+  .offset((page - 1) * perPage)
+  .get()
+  .then((snapshot) => {
+   let data = [];
+   snapshot.forEach((doc) => {
+    const selectedData = {
+       name: doc.data().title,
+       id: doc.data().id,
+       datePublished: doc.data().datePublished,
+       director: doc.data().director,
+       categories: doc.data().categories,
+       episode: doc.data().episode,
+       genres: doc.data().genres,
+       poster: doc.data().poster,
+       rating: doc.data().rating,
+       reviewCount: doc.data().reviewCount,
+       trailer: doc.data().trailer,
+       TMDB: doc.data().TMDB,
+      };
+
+      data.push(selectedData);
+    //data.push({ id: doc.id });
+   });
+
+   return Promise.all([data]);
+  })
+  .then(([data]) => {
+   res.json({ page, perPage, results: data });
+  })
+  .catch((err) => {
+   res.status(500).json({ error: err.message });
+  });
+
+
+/*
  (async () => {
   try {
    let query = db.collection("seies").limit(1);
@@ -52,52 +178,54 @@ router.get('/', (req, res) => {
    res.status(500).send({ status: "Failed", msg: error });
   }
  })();
-})
-
-router.get('/latest', (req, res) => {
- (async () => {
-  try {
-   let query = db.collection("seies").limit(1);
-   let response = [];
-
-   await db
-    .collection("series")
-    .orderBy("datePublished", "desc")
-    .limit(24)
-    .get()
-    .then((data) => {
-     let docs = data.docs;
-     docs.map((doc) => {
-      const selectedData = {
-       name: doc.data().title,
-       id: doc.data().id,
-       datePublished: doc.data().datePublished,
-       director: doc.data().director,
-       categories: doc.data().categories,
-       episode: doc.data().episode,
-       genres: doc.data().genres,
-       poster: doc.data().poster,
-       rating: doc.data().rating,
-       reviewCount: doc.data().reviewCount,
-       trailer: doc.data().trailer,
-       TMDB: doc.data().TMDB,
-      };
-
-      response.push(selectedData);
-     });
-
-     return response;
-    });
-
-   return res.status(200).send({ status: "Success", results: response });
-  } catch (error) {
-   console.log(error);
-   res.status(500).send({ status: "Failed", msg: error });
-  }
- })();
-})
+ */
+});
 
 router.get("/popular", (req, res) => {
+let page = req.query.page || 1;
+   if (isNaN(page) || page == "") page = 1;
+   else page = parseInt(page);
+   let perPage = req.query.perPage;
+   if (isNaN(perPage) || perPage == "") perPage = 24;
+   else perPage = parseInt(perPage);
+
+   let dataRef = db.collection("series");
+   let data = dataRef
+   .orderBy("rating", "desc")
+  .limit(perPage)
+  .offset((page - 1) * perPage)
+  .get()
+  .then((snapshot) => {
+   let data = [];
+   snapshot.forEach((doc) => {
+    const selectedData = {
+       name: doc.data().title,
+       id: doc.data().id,
+       datePublished: doc.data().datePublished,
+       director: doc.data().director,
+       categories: doc.data().categories,
+       episode: doc.data().episode,
+       genres: doc.data().genres,
+       poster: doc.data().poster,
+       rating: doc.data().rating,
+       reviewCount: doc.data().reviewCount,
+       trailer: doc.data().trailer,
+       TMDB: doc.data().TMDB,
+      };
+
+      data.push(selectedData);
+    //data.push({ id: doc.id });
+   });
+
+   return Promise.all([data]);
+  })
+  .then(([data]) => {
+   res.json({ page, perPage, results: data });
+  })
+  .catch((err) => {
+   res.status(500).json({ error: err.message });
+  });
+/*
  (async () => {
   try {
    let query = db.collection("seies").limit(1);
@@ -138,58 +266,124 @@ router.get("/popular", (req, res) => {
    res.status(500).send({ status: "Failed", msg: error });
   }
  })();
+ */
 });
 
 router.get("/show/:id", async function (request, res) {
-  try {
-   let response = [];
-let query = request.params.id
+ try {
+  let response = [];
+  let query = request.params.id;
 
-   await db
-    .collection("seriesData")
-    .where('id', '==', query)
-    
-    .limit(24)
-    .get()
-    .then((data) => {
-     let docs = data.docs;
-     docs.map((doc) => {
-      const selectedData = {
-       
-       id: doc.data().id,
-       seasons: doc.data().seasons
-      };
+  await db
+   .collection("seriesData")
+   .where("id", "==", query)
 
-      response.push(selectedData);
-     });
+   .limit(24)
+   .get()
+   .then((data) => {
+    let docs = data.docs;
+    docs.map((doc) => {
+     const selectedData = {
+      id: doc.data().id,
+      seasons: doc.data().seasons,
+     };
 
-     return response;
+     response.push(selectedData);
     });
-   //console.log(response)
-   //return response;
-   return res.status(200).send({ status: "Success", length:response.length, results: response });
-  } catch (error) {
-   console.log(error);
-   res.status(500).send({ status: "Failed", msg: error });
-  }
 
-  //return 'oke'
- });
+    return response;
+   });
+  //console.log(response)
+  //return response;
+  return res
+   .status(200)
+   .send({ status: "Success", length: response.length, results: response });
+ } catch (error) {
+  console.log(error);
+  res.status(500).send({ status: "Failed", msg: error });
+ }
+
+ //return 'oke'
+});
 router.get("/search", async function (request, res) {
-  try {
-   let response = [];
-let query = request.query.q
+ try {
+  let response = [];
+  let query = request.query.q;
 
-   await db
-    .collection("series")
-    .where('keywords', 'array-contains', query)
-    
-    .limit(24)
-    .get()
-    .then((data) => {
-     let docs = data.docs;
-     docs.map((doc) => {
-      const selectedData = {
+  await db
+   .collection("series")
+   .where("keywords", "array-contains", query)
+
+   .limit(24)
+   .get()
+   .then((data) => {
+    let docs = data.docs;
+    docs.map((doc) => {
+     const selectedData = {
+      name: doc.data().title,
+      id: doc.data().id,
+      datePublished: doc.data().datePublished,
+      director: doc.data().director,
+      categories: doc.data().categories,
+      episode: doc.data().episode,
+      genres: doc.data().genres,
+      poster: doc.data().poster,
+      rating: doc.data().rating,
+      reviewCount: doc.data().reviewCount,
+      trailer: doc.data().trailer,
+      TMDB: doc.data().TMDB,
+     };
+
+     response.push(selectedData);
+    });
+
+    return response;
+   });
+  //console.log(response)
+  //return response;
+  return res
+   .status(200)
+   .send({ status: "Success", length: response.length, results: response });
+ } catch (error) {
+  console.log(error);
+  res.status(500).send({ status: "Failed", msg: error });
+ }
+
+ //return 'oke'
+});
+
+router.get("/country/:country", async function (req, res) {
+ 
+  
+  let query = [];
+  if (req.params.country == "korea") {
+   query = ["Korea", "South Korea"];
+  } else if (req.params.country == "china") {
+   query = ["China"];
+  } else if (req.params.country == "west") {
+   query = ["West"];
+  } else if (req.params.country == "asian") {
+   query = ["Asian"];
+  }
+  
+  
+  let page = req.query.page || 1;
+   if (isNaN(page) || page == "") page = 1;
+   else page = parseInt(page);
+   let perPage = req.query.perPage;
+   if (isNaN(perPage) || perPage == "") perPage = 24;
+   else perPage = parseInt(perPage);
+
+   let dataRef = db.collection("series");
+   let data = dataRef
+   .where("categories", "array-contains-any", query)
+  .limit(perPage)
+  .offset((page - 1) * perPage)
+  .get()
+  .then((snapshot) => {
+   let data = [];
+   snapshot.forEach((doc) => {
+    const selectedData = {
        name: doc.data().title,
        id: doc.data().id,
        datePublished: doc.data().datePublished,
@@ -204,49 +398,99 @@ let query = request.query.q
        TMDB: doc.data().TMDB,
       };
 
-      response.push(selectedData);
-     });
+      data.push(selectedData);
+    //data.push({ id: doc.id });
+   });
 
-     return response;
+   return Promise.all([data]);
+  })
+  .then(([data]) => {
+   res.json({ page, perPage, results: data });
+  })
+  .catch((err) => {
+   res.status(500).json({ error: err.message });
+  });
+  
+  
+  
+  
+  
+  
+  
+  /*
+  
+  await db
+   .collection("series")
+   .where("categories", "array-contains-any", query)
+
+   .limit(24)
+   .get()
+   .then((data) => {
+    let docs = data.docs;
+    docs.map((doc) => {
+     const selectedData = {
+      name: doc.data().title,
+      id: doc.data().id,
+      datePublished: doc.data().datePublished,
+      director: doc.data().director,
+      categories: doc.data().categories,
+      episode: doc.data().episode,
+      genres: doc.data().genres,
+      poster: doc.data().poster,
+      rating: doc.data().rating,
+      reviewCount: doc.data().reviewCount,
+      trailer: doc.data().trailer,
+      TMDB: doc.data().TMDB,
+     };
+
+     response.push(selectedData);
     });
-   //console.log(response)
-   //return response;
-   return res.status(200).send({ status: "Success", length:response.length, results: response });
-  } catch (error) {
-   console.log(error);
-   res.status(500).send({ status: "Failed", msg: error });
+
+    return response;
+   });
+  //console.log(response)
+  //return response;
+  return res
+   .status(200)
+   .send({ status: "Success", length: response.length, results: response });
+
+
+ //return 'oke'
+*/
+});
+
+router.get("/genre/:genre", async function (req, res) {
+ 
+  let response = [];
+  let query = [];
+  if (req.params.genre == "horror") {
+   query = ["Horror"];
+  } else if (req.params.genre == "action") {
+   query = ["Action"];
+  } else if (req.params.genre == "romance") {
+   query = ["Romance"];
+  } else if (req.params.genre == "comedy") {
+   query = ["Comedy"];
   }
+  
+  
+  let page = req.query.page || 1;
+   if (isNaN(page) || page == "") page = 1;
+   else page = parseInt(page);
+   let perPage = req.query.perPage;
+   if (isNaN(perPage) || perPage == "") perPage = 24;
+   else perPage = parseInt(perPage);
 
-  //return 'oke'
- });
-
-router.get("/country/:country", async function (request, res) {
-  try {
-   let response = [];
-let query = []
-if (request.params.country == 'korea')
-{
- query = ['Korea', 'South Korea']
-} else if (request.params.country == 'china')
-{
- query = ['China']
-} else if (request.params.country == 'west')
-{
- query = ['West']
-} else if (request.params.country == 'asian')
-{
- query = ['Asian']
-}
-   await db
-    .collection("series")
-    .where('categories', 'array-contains-any', query)
-    
-    .limit(24)
-    .get()
-    .then((data) => {
-     let docs = data.docs;
-     docs.map((doc) => {
-      const selectedData = {
+   let dataRef = db.collection("series");
+   let data = dataRef
+   .where("categories", "array-contains-any", query)
+  .limit(perPage)
+  .offset((page - 1) * perPage)
+  .get()
+  .then((snapshot) => {
+   let data = [];
+   snapshot.forEach((doc) => {
+    const selectedData = {
        name: doc.data().title,
        id: doc.data().id,
        datePublished: doc.data().datePublished,
@@ -261,78 +505,56 @@ if (request.params.country == 'korea')
        TMDB: doc.data().TMDB,
       };
 
-      response.push(selectedData);
-     });
+      data.push(selectedData);
+    //data.push({ id: doc.id });
+   });
 
-     return response;
+   return Promise.all([data]);
+  })
+  .then(([data]) => {
+   res.json({ page, perPage, results: data });
+  })
+  .catch((err) => {
+   res.status(500).json({ error: err.message });
+  });
+  
+  
+  /*
+  await db
+   .collection("series")
+   .where("categories", "array-contains-any", query)
+
+   .limit(24)
+   .get()
+   .then((data) => {
+    let docs = data.docs;
+    docs.map((doc) => {
+     const selectedData = {
+      name: doc.data().title,
+      id: doc.data().id,
+      datePublished: doc.data().datePublished,
+      director: doc.data().director,
+      categories: doc.data().categories,
+      episode: doc.data().episode,
+      genres: doc.data().genres,
+      poster: doc.data().poster,
+      rating: doc.data().rating,
+      reviewCount: doc.data().reviewCount,
+      trailer: doc.data().trailer,
+      TMDB: doc.data().TMDB,
+     };
+
+     response.push(selectedData);
     });
-   //console.log(response)
-   //return response;
-   return res.status(200).send({ status: "Success", length:response.length, results: response });
-  } catch (error) {
-   console.log(error);
-   res.status(500).send({ status: "Failed", msg: error });
-  }
 
-  //return 'oke'
- });
+    return response;
+   });
+  //console.log(response)
+  //return response;
+  return res
+   .status(200)
+   .send({ status: "Success", length: response.length, results: response });
+*/
+});
 
-
-router.get("/genre/:genre", async function (request, res) {
-  try {
-   let response = [];
-let query = []
-if (request.params.genre == 'horror')
-{
- query = ['Horror']
-} else if (request.params.genre == 'action')
-{
- query = ['Action']
-} else if (request.params.genre == 'romance')
-{
- query = ['Romance']
-} else if (request.params.country == 'comedy')
-{
- query = ['Comedy']
-}
-   await db
-    .collection("series")
-    .where('categories', 'array-contains-any', query)
-    
-    .limit(24)
-    .get()
-    .then((data) => {
-     let docs = data.docs;
-     docs.map((doc) => {
-      const selectedData = {
-       name: doc.data().title,
-       id: doc.data().id,
-       datePublished: doc.data().datePublished,
-       director: doc.data().director,
-       categories: doc.data().categories,
-       episode: doc.data().episode,
-       genres: doc.data().genres,
-       poster: doc.data().poster,
-       rating: doc.data().rating,
-       reviewCount: doc.data().reviewCount,
-       trailer: doc.data().trailer,
-       TMDB: doc.data().TMDB,
-      };
-
-      response.push(selectedData);
-     });
-
-     return response;
-    });
-   //console.log(response)
-   //return response;
-   return res.status(200).send({ status: "Success", length:response.length, results: response });
-  } catch (error) {
-   console.log(error);
-   res.status(500).send({ status: "Failed", msg: error });
-  }
-
-  //return 'oke'
- });
-
-module.exports = router
+module.exports = router;
